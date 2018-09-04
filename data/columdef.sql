c83891a43734742e000404ff80d7d688b2ceba37
Eric <702042282@qq.com>
2018-09-04 10:54:03 +0800

    add database file

---
+++ data/columdef.sql
@@ -0,0 +1,6 @@
+INSERT INTO `columdef` VALUES ('型号', 'MACH', 100, 'true', 'true', 'plan_autod', '1');
+INSERT INTO `columdef` VALUES ('合格数', 'DONEqty', 100, 'true', 'true', 'plan_autod', '0');
+INSERT INTO `columdef` VALUES ('不良数', 'NGqty', 100, 'true', 'true', 'plan_autod', '0');
+INSERT INTO `columdef` VALUES ('目标产品', 'TARGETqty', 100, 'true', 'true', 'plan_autod', '0');
+INSERT INTO `columdef` VALUES ('剩余产品', 'Remainqty', 100, 'true', 'true', 'plan_autod', '0');
+INSERT INTO `columdef` VALUES ('时间', 'date', 110, 'true', 'true', 'plan_autod', '0');
---
+++ data/plan_ng.sql
@@ -0,0 +1,7 @@
+INSERT INTO `plan_ng` VALUES ('8', '0');
+INSERT INTO `plan_ng` VALUES ('9', '0');
+INSERT INTO `plan_ng` VALUES ('10', '1');
+INSERT INTO `plan_ng` VALUES ('11', '0');
+INSERT INTO `plan_ng` VALUES ('12', '3');
+INSERT INTO `plan_ng` VALUES ('13', '0');
+INSERT INTO `plan_ng` VALUES ('12', '222');
